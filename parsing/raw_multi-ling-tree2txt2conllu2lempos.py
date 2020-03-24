d6760d8ea7eef5988bc16305a5c9a0d7d8d013fe
u2 <mkunilovskaya@gmail.com>
2020-03-24 16:18:01 +0000

    structuring

---
+++ parsing/raw_multi-ling-tree2txt2conllu2lempos.py
@@ -0,0 +1,117 @@
+# coding: utf-8
+'''
+pre-process multiling tree of folders with raw texts (not necessarily one-sent per line)
+into one-sent-per-line punct-tokenized txt, conllu corpus and a lempos corpus
+
+use available filters to adjust the required format for each of the three outputs
+the script expects:
+(1) a path to the root of the tree (last folder has lang index as the name) to be preprocessed
+(2) a path where to store the output; you don't have to create it, just say where to create it
+(3) the UD models for en, ru in the working folder (from which this script is run)
+(4) the preprocess_imports.py module with the functions to be imported
+(5) use conllu_only = True switch to reduce the output to conllu only
+
+USAGE: python3 raw2txt2conllu2lempos.py --texts data/raw/ --outto data/parsed/ --lang en
+'''
+import sys
+import argparse
+import zipfile, io, sys
+from preprocess_imports import * #tokeniseall, unify_sym, postprocess_ud, do_job
+from smart_open import open
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--texts', help="Path to a folder (or a tree of folders) of prepared txt files", required=True)
+# processed makes sense as a dirname
+parser.add_argument('--outto', help="Path to the root folder to hold the resulting tree ending with sent_tok/, conllu/ and lempos/ subs", required=True)
+
+
+args = parser.parse_args()
+
+start = time.time()
+
+rootout = args.outto
+os.makedirs(rootout, exist_ok=True)
+
+# do you really want all three formats of your corpus? I don't think so
+conllu_only = True
+
+# specify models for your languages (and stoplists, if required)
+# # STOPWORDS_FILE = 'stopwords_ru'
+# # stopwords = set([w.strip().lower() for w in smart_open(STOPWORDS_FILE,'r').readlines()])
+ru_udpipe_filename = 'udpipe_syntagrus.model'  ## I am getting permission denied on this model
+ru_model = Model.load(ru_udpipe_filename)
+ru_pipeline = Pipeline(ru_model, 'tokenize', Pipeline.DEFAULT, Pipeline.DEFAULT, 'conllu')
+
+# # STOPWORDS_FILE = 'stopwords_ru'
+# # stopwords = set([w.strip().lower() for w in smart_open(STOPWORDS_FILE,'r').readlines()])
+en_udpipe_filename = 'english-ewt-ud-2.3-181115.udpipe'
+en_model = Model.load(en_udpipe_filename)
+en_pipeline = Pipeline(en_model, 'tokenize', Pipeline.DEFAULT, Pipeline.DEFAULT, 'conllu')
+
+# # functional = set('ADP AUX CCONJ DET PART PRON SCONJ PUNCT'.split())
+counter = 0
+for subdir, dirs, files in os.walk(args.texts):
+    for id, f in enumerate(files):
+        last_folder = subdir + os.sep
+        lang_folder = len(os.path.abspath(last_folder).split('/')) - 1
+        lang = os.path.abspath(last_folder).split('/')[lang_folder]
+    
+        if conllu_only:
+            out = rootout + '/'.join(last_folder.split('/')[-4:-1]) + '/'
+            os.makedirs(out, exist_ok=True)
+            counter += 1
+            filepath = subdir + os.sep + f
+            with open(filepath, 'r', errors='ignore') as input_text:
+                ud_outf = out + f.replace('.txt', '.conllu')
+    
+                try:
+                    text = input_text.read().strip()
+                except UnicodeDecodeError:
+                    print(f)
+                    continue
+                if lang == 'en':
+                    do_conllu_only(en_pipeline, text, lang, ud_outf)
+                elif lang == 'ru':
+                    do_conllu_only(ru_pipeline, text, lang, ud_outf)
+    
+                ### Monitor processing:
+                if counter % 50 == 0:
+                    print('%s files processed' % counter, file=sys.stderr)
+        else:
+            out = rootout + '/'.join(last_folder.split('/')[-4:-1])
+            os.makedirs(out, exist_ok=True)
+            
+            txt_out = out + '/sent_tok/'
+            os.makedirs(txt_out, exist_ok=True)
+            
+            conllu_out = out + '/conllu/'
+            os.makedirs(conllu_out, exist_ok=True)
+            
+            lempos_out = out + '/lempos/'
+            os.makedirs(lempos_out, exist_ok=True)
+
+            counter += 1
+            filepath = subdir + os.sep + f
+            with open(filepath, 'r', errors='ignore') as input_text:
+                txt_outf = txt_out + f
+                ud_outf = conllu_out + f.replace('.txt', '.conllu')
+                temp_outf = out + f.replace('.txt', '.temp')
+                lempos_outf = lempos_out + f.replace('.txt', '.lempos')
+    
+                try:
+                    text = input_text.read().strip()
+                except UnicodeDecodeError:
+                    print(f)
+                    continue
+                if lang == 'en':
+                    do_job(en_pipeline, text, lang, txt_outf, ud_outf, temp_outf, lempos_outf, txt_sents=True)
+                elif lang == 'ru':
+                    do_job(ru_pipeline, text, lang, txt_outf, ud_outf, temp_outf, lempos_outf, txt_sents=True)
+                    
+                ### Monitor processing:
+                if counter % 50 == 0:
+                    print('%s files processed' % counter, file=sys.stderr)
+
+end = time.time()
+processing_time = int(end - start)
+print('Processing %s (%s files) took %.2f minites' % (args.texts, counter, processing_time / 60), file=sys.stderr) #.split('/')[-2]
