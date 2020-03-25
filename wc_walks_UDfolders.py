6bf08715f2f6478c058fe04a11295c5dd141ed1c
u2 <mkunilovskaya@gmail.com>
2020-03-25 00:02:32 +0000

    getting stats from ultimately filtered conllu

--- wc_walks_UDfolders.py
+++ wc_walks_UDfolders.py
@@ -13,23 +13,20 @@ def wordcount(trees):
 
     return words
 
-def get_trees(data): # data is one object: a text or all of corpus as one file
-    sentences = []
+
+def get_trees(data):  # data is one object: a text or all of corpus as one file
+    sentences0 = []
+    badsents = 0
     only_punct = []
-    current_sentence = []  # определяем пустой список
-    for line in data:  # fileinput.input():  # итерируем строки из обрабатываемого файла
-        if line.strip() == '':  # что делать есть строка пустая (это граница предложения!):
-            if current_sentence:  # и при этом в списке уже что-то записано
-                sentences.append(current_sentence)
-            # if only_punct:
-            # 	# if set(only_punct) == 2:
-            # 		print('GOTCHA', only_punct)
-            current_sentence = []  # обнуляем список
+    current_sentence = []
+    for line in data:
+        if line.strip() == '':
+            if current_sentence:
+                sentences0.append(current_sentence)
+            
+            current_sentence = []
             only_punct = []
-            # if the number of sents can by divided by 1K without a remainder.
-            # В этом случае, т.е. после каждого 1000-ного предложения печатай месседж. Удобно!
-            #         if len(sentences) % 1000 == 0:
-            #             print('I have already read %s sentences' % len(sentences))
+            
             continue
         if line.strip().startswith('#'):
             continue
@@ -37,52 +34,46 @@ def get_trees(data): # data is one object: a text or all of corpus as one file
         (identifier, token, lemma, upos, xpos, feats, head, rel, misc1, misc2) = res
         if '.' in identifier or '-' in identifier:  # ignore empty nodes possible in the enhanced representations
             continue
-        # во всех остальных случаях имеем дело со строкой по отдельному слову
-        # in ref_RU data there are sentences that consist of 4 PUNCTs only!
+        
         for i in res:
-            # print(res)
             only_punct.append(res[3])
         var = list(set(only_punct))
-        # throw away sentences that consist of just PUNCT, particularly rare 4+ PUNCT
+        # throw away sentences consisting of punctuation marks only (ex. '.)') and of numeral and a punctuation (ex. '3.', 'II.')
         if len(var) == 1 and var[0] == 'PUNCT':
-            # print('GOTCHA', set(only_punct))
             continue
+        if len(var) == 2 and 'PUNCT' in var and 'NUM' in var:
+            badsents += 1
         else:
             current_sentence.append((int(identifier), token, lemma, upos, xpos, feats, int(head), rel))
-    ## это записывает последнее предложение, не заканчиающееся пустой строкой? НЕЕЕТ, там есть пустая строка
+    
     if current_sentence:
-        # if len(current_sentence) < 3:
-        # 	print('+++', current_sentence)
-        sentences.append(current_sentence)  # получаем список предложений из файла
-
-    sentences = [s for s in sentences if len(s) >= 4] ### here is where I filter out short sents
-    # print('==', len(sentences))
-    return sentences
-
-
-# In[38]:
+        sentences0.append(current_sentence)
+    
+    sentences = [s for s in sentences0 if len(s) >= 4]
+    shorts = len(sentences0) - len(sentences)
+    
+    return sentences, badsents, shorts
 
 
 def get_meta(input):
-    #/home/masha/02targets_wlv/data/croco/pro/de
     # prepare for writing metadata
     lang_folder = len(os.path.abspath(input).split('/')) - 1
     status_folder = len(os.path.abspath(input).split('/')) - 2
-    korp_folder = len(os.path.abspath(input).split('/')) - 3
-
+    register_folder = len(os.path.abspath(input).split('/')) - 3
+    
     status0 = os.path.abspath(input).split('/')[status_folder]
-    korp0 = os.path.abspath(input).split('/')[korp_folder]
-    # register0 = os.path.abspath(input).split('/')[reg_folder]
+    register0 = os.path.abspath(input).split('/')[register_folder]
+    
     lang0 = os.path.abspath(input).split('/')[lang_folder]
-
-    return lang0,korp0,status0 ##register0,
-
-
-# In[39]:
-
+    
+    return lang0, register0, status0
 
 tot_wc = defaultdict(int)
-languages = ['en', 'de', 'ru']
+tot_sents = defaultdict(int)
+languages = ['en', 'ru'] # de
+
+tot_bad = 0
+tot_short = 0
 
 for subdir, dirs, files in os.walk(rootdir):
 
@@ -95,11 +86,11 @@ for subdir, dirs, files in os.walk(rootdir):
 
         language = os.path.abspath(last_folder).split('/')[lang_folder]
 
-        # data hierachy: /your/path/ol/croco/pro/de/*.conllu
+        # data hierachy: /your/path/to/corpus/fiction/source/en/*.conllu
 
         # this collects counts for every sentence in a document
         # prepare for writing metadata:
-        lang, korp, status = get_meta(last_folder)
+        lang, reg, status = get_meta(last_folder)
         meta_str = '_'.join(get_meta(last_folder)) # lang, register, korp, status
 
 #         if i % 20 == 0:
@@ -110,13 +101,23 @@ for subdir, dirs, files in os.walk(rootdir):
 
         data = open(filepath).readlines()
 
-        corp_id = lang + '_' + status + '_' + korp
-        sents = get_trees(data)
-        
+        corp_id = lang + '_' + status + '_' + reg
+        sents, bad, short = get_trees(data)
+        tot_bad += bad
+        tot_short += short
+    
         normBy_wc = wordcount(sents)
         tot_wc[corp_id] += normBy_wc
-    
+        tot_sents[corp_id] += len(sents)
 
+print('Word counts by subcorpus:')
 for k, v in tot_wc.items():
     print(k, v)
 
+print()
+print("Number of dentences:")
+for k, v in tot_sents.items():
+    print(k, v)
+    
+print('Sentence splitting errors:', tot_bad)
+print('Short sentences:', tot_short)
