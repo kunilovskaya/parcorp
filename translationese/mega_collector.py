13f5c21590b96082829f3c0b94ff11b2872b3223
u2 <mkunilovskaya@gmail.com>
2020-04-27 12:18:38 +0100

    readme update

--- translationese/mega_collector.py
+++ translationese/mega_collector.py
@@ -6,12 +6,11 @@ expected structure of folders: we store names of the last three folders as level
 (2) the last but one is status (professional/student, reference, source (in the current project it duplicates the lang folder for the sake of structure)
 (3) the last but two is source of data (authors, corpora, genres/registers)
 For example:
-# data hierachy: /your/path/anylength/parsed/register/status/lang/*.conllu, where parsed is the name of the input folder
+data hierachy: /your/path/anylength/parsed/register/status/lang/*.conllu, where parsed is the name of the input folder
 
 USAGE (from extract_translationese_features folder!): python3 mega_collector.py --input parsed/ --output mockdata.tsv --langs en ru
 '''
 
-import os, sys
 import csv
 from extractors import *
 
@@ -19,7 +18,6 @@ import argparse
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--input', help="Path to the tree of folders with *.conllu named after your text categories", required=True)
-# processed makes sense as a dirname
 parser.add_argument('--output', default='mock_data.tsv', help="Path to, and name of, the resulting spreadsheet")
 parser.add_argument('--langs', nargs='+', default=['en','ru'], help='Pass language indices like so: --langs en ru')
 
@@ -29,11 +27,11 @@ input_dir = args.input  # parsed/
 outname = args.output  # mock_data.tsv
 
 # here, for each file we collect counts averaged over number of words or number of sentences
-## muted features: passives interrog andor wdlength mark nn
+# muted features: bypassives shorpassives andor wdlength mark nn
 keys = 'afile alang aregister astatus ' \
        'sentlength ppron possdet indef cconj whconj relativ pied correl copula ' \
        'attrib pasttense lexdens lexTTR mquantif mpred finites infs pverbals deverbals ' \
-       'bypassives shortpassives sconj addit advers caus tempseq epist but comp ' \
+       'passives interrog sconj addit advers caus tempseq epist but comp ' \
        'sup neg numcls simple demdets nnargs mhd mdd acl aux ' \
        'aux:pass ccomp nsubj:pass parataxis xcomp'.split()
 master_dict = {k: [] for k in keys}
@@ -101,8 +99,9 @@ for subdir, dirs, files in os.walk(input_dir):
             print('I have processed %s files from %s' % (i, meta_str.upper()), file=sys.stderr)
             print('I have ignored %d all-punct-num sentences and %d less-than-4-words sents\n' % (
             tot_bads, tot_shorts))
-        # don't forget the filename
-        doc = os.path.splitext(os.path.basename(last_folder + filepath))[0]  # without extention
+            
+        # don't forget the filename without extention
+        doc = os.path.splitext(os.path.basename(last_folder + filepath))[0]
         
         data = open(filepath).readlines()
         
@@ -123,7 +122,7 @@ for subdir, dirs, files in os.walk(input_dir):
         normBy_sentnum = sents_num(sents, language)
         normBy_verbnum = verbs_num(sents, language)
         
-        ## create text-level counters for each feature values
+        # create text-level counters for each feature values
         ppron_res = 0
         possdet_res = 0
         anysome_res = 0
@@ -135,7 +134,7 @@ for subdir, dirs, files in os.walk(input_dir):
         correl_res = 0
         copula_res = 0
         # wdlength_res = 0
-        # interrog_res = 0
+        interrog_res = 0
         # nn_res = 0
         attrib_res = 0
         pasttense_res = 0
@@ -144,17 +143,16 @@ for subdir, dirs, files in os.walk(input_dir):
         mpred_res = 0
         mquantif_res = 0
         finites_res = 0
-        ### adding July2019 features
         infs_res = 0
         pverbals_res = 0
-        # passives_res = 0
-        bypassives_res = 0
-        shortpassives_res = 0
+        passives_res = 0
+        # bypassives_res = 0
+        # shortpassives_res = 0
         
         speakdiff_res = 0
         readerdiff_res = 0
         
-        ## text-level counts
+        # text-level counts
         deverbals_res = nominals(sents, language, pseudo_deverbs, vconverts)
         
         addit_res = count_dms(additive, sents, language)
@@ -166,7 +164,7 @@ for subdir, dirs, files in os.walk(input_dir):
         # andor_res = and_or_counts(sents, language)
         but_res = but_counts(sents, language)
         
-        ## counts for degrees of comparison normalized internally for num of adj+adv
+        # counts for degrees of comparison normalized internally for num of adj+adv
         comp_res, sup_res = comparison_degrees(sents, language, fn=file)
         
         neg_res = polarity(sents, language)
@@ -177,7 +175,7 @@ for subdir, dirs, files in os.walk(input_dir):
         demdets_res = demdeterm(sents, language)
         nnargs_res = nouns_to_all(sents)
         
-        ## run functions and get absolute freqs for each text
+        # run functions and get absolute freqs for each text
         for sent in sents:
             ppron_res += prsp(sent, language)[0]
             possdet_res += possdet(sent, language)[0]
@@ -195,7 +193,7 @@ for subdir, dirs, files in os.walk(input_dir):
             correl_res += correlat
             copula_res += copulas(sent)
             # wdlength_res += word_length(sent)
-            # interrog_res += interrog(sent, language)[0]
+            interrog_res += interrog(sent, language)[0]
             # nn_res += nn(sent, language)[0]
             attrib_res += attrib(sent)[0]
             pasttense_res += pasttense(sent)
@@ -208,9 +206,9 @@ for subdir, dirs, files in os.walk(input_dir):
             infs_res += infinitives(sent, language, mpred_support)
             pverbals_res += participles(sent, language)
             bys, nobys = passives(sent, language)
-            # passives_res += (bys+nobys)
-            bypassives_res += bys
-            shortpassives_res += nobys
+            passives_res += (bys+nobys)
+            # bypassives_res += bys
+            # shortpassives_res += nobys
         # add functions here, except for these that operate on the text, rather than sentence level
         
         # run functions that are doc(file)-level
@@ -219,7 +217,7 @@ for subdir, dirs, files in os.walk(input_dir):
         current['sentlength'] = avsents
         
         # normalisation for the absolute sentence-level freqs is done mostly(NB!) in two different ways:
-        ## by number of words in the text
+        # by number of words in the text
         current['ppron'] = ppron_res / normBy_wc
         current['possdet'] = possdet_res / normBy_wc
         current['indef'] = anysome_res / normBy_wc
@@ -234,28 +232,30 @@ for subdir, dirs, files in os.walk(input_dir):
         
         current['mpred'] = mpred_res / normBy_sentnum  # need to normalize to number of finites when I get it :-)
         
-        ## by number of sentences
+        # by number of sentences
         current['mhd'] = speakdiff_res / normBy_sentnum
         current['mdd'] = readerdiff_res / normBy_sentnum
         current['relativ'] = relativ_res / normBy_sentnum
         current['pied'] = pied_res / normBy_sentnum
         current['correl'] = correl_res / normBy_sentnum
         current['copula'] = copula_res / normBy_sentnum
-        # current['interrog'] = interrog_res / normBy_sentnum
+        current['interrog'] = interrog_res / normBy_sentnum
         current['attrib'] = attrib_res / normBy_sentnum
         current['pasttense'] = pasttense_res / normBy_sentnum
         # current['wdlength'] = wdlength_res / normBy_sentnum
         current['finites'] = finites_res / normBy_verbnum
         
-        ## alternatively normalize these three features by number of verbs using / normBy_verbnum
+        # alternatively normalize these three features by number of verbs using / normBy_verbnum
         current['infs'] = infs_res / normBy_verbnum
         current['pverbals'] = pverbals_res / normBy_verbnum
         current['deverbals'] = deverbals_res / normBy_verbnum
         
-        # current['passives'] = passives_res / normBy_sentnum
-        current['bypassives'] = bypassives_res / normBy_sentnum  ## maybe these two need to be counted as one feature
-        current['shortpassives'] = shortpassives_res / normBy_sentnum
-        ## here are counts for 5 semantic groups of DMs + conts for and/or and but (maybe merge them!)
+        current['passives'] = passives_res / normBy_sentnum
+        # maybe these two need to be counted as one feature
+        # current['bypassives'] = bypassives_res / normBy_sentnum
+        # current['shortpassives'] = shortpassives_res / normBy_sentnum
+        
+        # here are counts for 5 semantic groups of DMs + conts for and/or and but (maybe merge them!)
         current['addit'] = addit_res / normBy_sentnum
         current['advers'] = advers_res / normBy_sentnum
         current['caus'] = caus_res / normBy_sentnum
@@ -271,7 +271,7 @@ for subdir, dirs, files in os.walk(input_dir):
         current['demdets'] = demdets_res / normBy_wc
         current['nnargs'] = nnargs_res
         
-        ## add 7 UD features from a dict
+        # add 7 UD features from a dict
         dep_prob_dict = ud_probabilities(sents, language)
         for k, val in dep_prob_dict.items():
             current[k] = val
